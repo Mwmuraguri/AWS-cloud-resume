ed6ed776bcd881b7c998783308ec72da373f4cd7
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-12-02 22:24:12 +0300

    Updates

--- svg/index.js
+++ svg/index.js
@@ -1,8 +1,26 @@
 const counter = document.querySelector(".counter-number");
+
 async function updateCounter() {
-    let response = await fetch("https://ebt2idsxwlt4lwy3y34g7far440wyhwc.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
+    try {
+        // Fetch data from the server
+        let response = await fetch("https://fu6pt364bbesypsehh75yvcosa0bnxdq.lambda-url.us-east-1.on.aws/");
+
+        // Check if the request was successful (status code 200)
+        if (response.ok) {
+            // Parse the response as JSON
+            let data = await response.json();
+
+            // Update the counter with the retrieved data
+            counter.innerHTML = `This page has ${data} Views!`;
+        } else {
+            // Handle error response
+            console.error(`Failed to fetch visit count. Status: ${response.status}`);
+        }
+    } catch (error) {
+        // Handle fetch errors
+        console.error("Error fetching visit count:", error);
+    }
 }
 
+// Call the function to update the counter when the page loads
 updateCounter();
